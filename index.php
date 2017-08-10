bd5131965a994ffe7cf4d60d7a855c7fb6b1bdb2
Charlie Hewitt <contact@chewitt.me>
2017-08-10 19:35:56 +0100

    twig initial

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<?php
+
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
+include 'portfolio.php';
+
+?>
+
+<head>
+    <title>Charlie Hewitt</title>
+    <meta name="description" content="Charlie Hewitt's personal site."></meta>
+    <meta name="keywords" content="charlie hewitt"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Styles/normalize.css" type="text/css">
+    <link rel="stylesheet" href="Styles/style.css" type="text/css">
+    <script src="Scripts/main.js" type="text/javascript"></script>
+</head>
+
+<body>
+    <div id="top">
+        <div style="height:2rem"></div>
+        <div class="header-bg"></div>
+        <div class="header">
+            <div class="content">
+                <h1>Charlie Hewitt</h1>
+            </div>
+        </div>
+        <div class="padder"></div>
+        <nav class="menu">
+            <div class="content">
+                <div class="hamburger" onclick="showMenu()">&#9776;</div>
+                <ul class="menu-ul">
+                    <a href="#portfolio" onclick="goToPortfolio();return false;">
+                        <li>Portfolio</li>
+                    </a>
+                    <a href="#other" onclick="goToAbout();return false;">
+                        <li>About</li>
+                    </a>
+                    <a href="mailto:contact@chewitt.me">
+                        <li class="contact">Contact</li>
+                    </a>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <div class="container">
+        <section id="portfolio">
+            <div class="content">
+                <?php
+                    foreach ($portfolio_items as $item) {
+                        echo $twig->render('portfolio_item.html', $item);
+                    }
+                ?>
+            </div>
+        </section>
+        <section id="other">
+            <img src="pic.jpg" class="pic" />
+            <p>I'm a <?php
+                    $bd = new DateTime('1995-09-13');
+                    $diff = $bd->diff(new DateTime());
+                    echo $diff->y;
+                ?> year old student living near London in the UK.</p>
+            <p>I'm studying for a MEng in computer science at the University of Cambridge.</p>
+            <p>I like to design and code things - apps, websites, iOS tweaks, icons &amp; interfaces.</p>
+            <p>I also love to play and write music, take photos &amp; row.</p>
+            <p>A full CV is available <a href="cv.html" target="_blank">here</a>.</p>
+            <a class="btn white" href="mailto:contact@chewitt.me" target="_blank">
+                Get in touch
+            </a>
+        </section>
+        <ul class="social-icons">
+            <a href="http://twitter.com/friggog" target="_blank">
+                <li>
+                    <svg class="padded" viewBox="0 0 800 800">
+                        <path d="M679 239s-21 34-55 57c7 156-107 329-314 329-103 0-169-50-169-50s81 17 163-45c-83-5-103-77-103-77s23 6 50-2c-93-23-89-110-89-110s23 14 50 14c-84-65-34-148-34-148s76 107 228 116c-22-121 117-177 188-101 37-6 71-27 71-27s-12 41-49 61c30-2 63-17 63-17z"/>
+                    </svg>
+                </li>
+            </a>
+            <a href="https://uk.linkedin.com/in/charlie-hewitt-48a175110" target="_blank">
+                <li>
+                    <svg class="padded" viewBox="0 0 800 800">
+                        <path d="M268 629h-97V319h97zm157 0h-97V319h93v42h1q31-50 93-50 114 0 114 133v185h-96V466q0-70-49-70-59 0-59 69z"/>
+                        <circle cx="219" cy="220" r="56"/>
+                    </svg>
+                </li>
+            </a>
+            <a href="http://github.com/friggog" target="_blank">
+                <li>
+                    <svg viewBox="0 0 800 800">
+                        <path d="M400 139c144 0 260 116 260 260 0 115-75 213-178 247-9 3-17-2-17-13v-71c0-35-18-48-18-48 57-6 119-28 119-128 0-44-27-70-27-70s14-29-2-69c0 0-22-7-72 27-42-12-88-12-130 0-50-34-72-27-72-27-16 40-2 69-2 69s-27 26-27 70c0 100 62 122 119 128 0 0-14 10-17 35-15 7-53 18-76-22 0 0-13-25-39-27 0 0-26 0-2 16 0 0 17 8 29 38 0 0 16 51 88 35v44c0 11-9 16-18 13-103-34-178-132-178-247 0-144 116-260 260-260z"/>
+                    </svg>
+                </li>
+            </a>
+        </ul>
+        <div class="copy">&copy; 2017</div>
+    </div>
+</body>
+
+</html>
---
+++ portfolio.php
@@ -0,0 +1,155 @@
+<?php
+$portfolio_items = array(
+    array(
+        'title' => 'Cydar',
+        'button_link' => 'http://www.cydarmedical.com',
+        'button_title' => 'Website',
+        'content' => '<p>I\'ll be joining the team at Cydar in Cambridge for a two month internship over the summer of 2017.</p>
+                      <p>I will help to develop technologies for medical imaging to be used by surgeons during operations.</p>'
+    ),
+    array(
+        'title' => 'Part II Project',
+        'image' => 'Trees',
+        'button_link' => './CTH-Dissertation-2017.pdf',
+        'button_title' => 'Read Paper',
+        'content' => '<p>For my third year project I\'ve explored the procedural modelling of trees for use in computer graphics.</p>
+                      <p>I\'ve implemented two systems which generate tree models of many types within the blender modelling application.</p>
+                      <p>Source code available on <a href="https://github.com/friggog/tree-gen" target="_blank">GitHub</a>.</p>'
+
+    ),
+    array(
+        'title' => 'ML &amp; AI',
+        'image' => 'AI',
+        'content' => '<p>As part of my studies I\'ve been learning a lot about machine learning and artifical intelligence, so I thought I\'d have a go at implementing some of the concepts.</p>
+                      <p>So far I\'ve implemented a Bayesian neural network, Gaussian process regression (pictured) and the Viterbi algorithm - I\'m hoping to implement a support vector machine and more soon!</p>'
+    ),
+    array(
+        'title' => 'PidgePing',
+        'content' => '<p>Project completed as part of the Hack Cambridge hackathon in January 2017.</p>
+                      <p>An IR sensor placed in the user\'s mailbox would communicate via WIFI to our server which then sent out an email notifying them. Ideal for Cambridge students whose mailboxes are often far away from where they live.</p>
+                      <p>A website allowed users to register and set up their device, as well as check its status.</p>'
+    ),
+    array(
+        'title' => 'OpenGL Shaders',
+        'image' => 'Shaders',
+        'button_link' => './Shaders/',
+        'button_title' => 'Read More',
+        'content' => '<p>Playing around with some space themed planet shaders in GLSL.</p>
+                      <p>Including:</p>
+                      <ul>
+                          <li>Poisson distributed galactic environment generation.</li>
+                          <li>Solar system generation with dynamic lighting.</li>
+                          <li>Earth-like planet, gas planet and star shaders.</li>
+                          <li>Pixel art shader.</li>
+                      </ul>'
+    ),
+    array(
+        'title' => 'Jagex',
+        'button_link' => 'http://www.jagex.com',
+        'button_title' => 'Website',
+        'content' => '<p>I\'m joining the team at Jagex Games Studio for three months in the summer of 2016 as an intern.</p>
+                      <p>I\'ll be working on a project as part of a small team aimed at exploring new areas which Jagex may be interested in expanding into in the future.</p>'
+    ),
+    array(
+        'title' => 'SplitBit',
+        'content' => '<p>SplitBit is an online equity, salary and payments negotiation platform aimed at startups, particularly in the tech industry.</p>
+                      <p>The site was created as part of the Cambridge CST group project programme. The group worked with clients Matt Johnson and Eddy Ashton from <a href="http://www.frontier.co.uk/" target="=_blank">Frontier</a>, a games company based in Cambridge.</p>'
+    ),
+    array(
+        'title' => 'THBC Site Redesign',
+        'image' => 'THBC',
+        'button_link' => 'http://thbc.soc.srcf.net/',
+        'button_title' => 'View Website',
+        'content' => '<p>Trinity Hall Boat Club\'s original website was designed in the 90s and was starting to show its age. I carried out a complete visual redesign and tidied up many aspects of the website code.</p>
+                      <p>I also wrote a new voting system to be used for captaincy elections, as well as updating the content of many of the site\'s pages and adding some new ones.</p>'
+    ),
+    array(
+        'title' => 'Fingal',
+        'button_link' => 'http://cydia.saurik.com/package/me.chewitt.fingal/',
+        'button_title' => 'More Info',
+        'content' => '<p>Animated icon theming platform (similar to Winterboard) for iOS 8 and 9.</p>
+                      <p>Available for free from the ModMyi Cydia repo. Source code available <a href="https://github.com/friggog/Fingal" target="=_blank">on github</a>.</p>'
+    ),
+    // array(
+    //     'title' => 'JS Raytracer',
+    //     'image' => 'Raytracer',
+    //     'button_link' => './work/raytracer',
+    //     'button_title' => 'Preview',
+    //     'content' => '<p>A (quite slow) Raytracer written using javascript and rendered using HTML5 canvas.</p>
+    //                   <p>Uses Phong illumination with recursive reflection and coloured lighting/shapes. Can render planes, triangles and perfect spheres with varying shininess and reflectivity.</p>'
+    // ),
+    // array(
+    //     'title' => 'WebGL Earth',
+    //     'image' => 'Earth',
+    //     'button_link' => './work/webgl',
+    //     'button_title' => 'Preview',
+    //     'content' => '<p>First experiment writing custom OpenGL shaders using WebGL and THREE.js.</p>
+    //                   <p>Model of Earth with Texture, Normal, Displacement and Specular mapping and Phong illumination.</p>'
+    // ),
+    array(
+        'title' => 'seng',
+        'image' => 'Seng',
+        'button_link' => 'http://cydia.saurik.com/package/me.chewitt.seng/',
+        'button_title' => 'More Info',
+        'content' => '<p>Seng is a sleek and highly customisable replacement for your iOS 8 &amp; 9 app switcher and control center, providing 2 core features; Multi Centre and Hot Corners.</p>
+                      <p>Available to purcahse from the Cydia Store.</p>'
+    ),
+    array(
+        'title' => 'CustomCover',
+        'button_link' => 'http://cydia.saurik.com/package/me.chewitt.customcover/',
+        'button_title' => 'More Info',
+        'content' => '<p>Customise the iOS LockScreen Now Playing view and Music app. Packed full of features and compatible with all devices running iOS 7 through 9.</p>
+                      <p>Available to purcahse from the Cydia Store.</p>
+                      <p><a href="http://chewitt.me/Info/CustomCoverAPI.html" target="_blank">Developer Info</a></p>'
+    ),
+    array(
+        'title' => 'Messages Customiser',
+        'image' => 'MC',
+        'button_link' => 'http://cydia.saurik.com/package/me.chewitt.mcpro/',
+        'button_title' => 'More Info',
+        'content' => '<p>The most fully featured tweak available for modifying the iOS Messages app. Messages Customiser is the one stop solution for tweaking your texting experience on all devices runnning iOS 7 - 9</p>
+                      <p>Free and Premium version available from the Cydia Store.</p>'
+    ),
+    array(
+        'title' => 'Subtle7',
+        'content' => '<p>Simple winterboard theme with subtle changes from the default iOS 7 look. Inspired by the work of <a href="http://louie.land" target="_blank">Louie Mantia</a></p>
+                      <p>Includes all stock icons, and a few extra. Compatible with all devices on iOS 7.</p>
+                      <p>Available from my <a href="http://chewitt.me/repo" target="_blank">personal repo</a>.</p>'
+    ),
+    array(
+        'title' => 'Tinct &amp; Chroma',
+        'image' => 'Tinct',
+        'content' => '<p>Chroma, and the more fully featured Tinct, are a duo of UI tweaks for iOS 7 and 8. Both allow for extensive customisation of the colours of various elements of the iOS interface, while remaining lightweight and simple to use.</p>
+                      <p>Chroma is available for free from Cydia (source on <a href="https://github.com/friggog/chroma">GitHub</a>), and Tinct is available to purchase from the Cydia Store.</p>'
+    ),
+    array(
+        'title' => 'Linen Growl Style',
+        'image' => 'Linen',
+        'button_link' => 'http://friggog.deviantart.com/art/Linen-GrowlStyle-292018277',
+        'button_title' => 'Download',
+        'content' => '<p>Custom style for the popular Growl notification management app.</p>
+                      <p>Inspired by the iOS 5 and OSX Mountain Lion interface styling and with custom slide in animation.</p>'
+    ),
+    array(
+        'title' => 'Flippers Ain\'t Wings',
+        'image' => 'Flippers',
+        'button_link' => './Flippers/',
+        'button_title' => 'More Info',
+        'content' => '<p>Block jumping, arcade style game for all iOS devices.</p>
+                      <p>Follow the story of Percy the Penguin in his adventure to discover the source of mysterious ice blocks falling from the sky.</p>'
+    ),
+    array(
+        'title' => 'Tryst',
+        'content' => '<p>Winterboard theme for retina iPad on iOS 6.</p>
+                      <p>Includes:</p>
+                      <ul>
+                          <li>All stock icons</li>
+                          <li>AppStore Icon Mask</li>
+                          <li>Springboard Elements</li>
+                          <li>App Switcher</li>
+                          <li>Notification Centre</li>
+                      </ul>
+                      <p>Available from the Cydia Store</p>'
+    ),
+);
+?>
