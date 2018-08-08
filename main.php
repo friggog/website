b16400f2737d9a0dc5605b168558e9c6aecf6165
Charlie Hewitt <contact@chewitt.me>
2018-08-08 20:15:10 +0100

    style fix

--- main.php
+++ main.php
@@ -31,7 +31,7 @@ $social_icons = array(
 
 <head>
     <title>Charlie Hewitt</title>
-    <meta name="description" content="Charlie Hewitt's personal site."></meta>
+    <meta name="description" content="Charlie Hewitt's personal website."></meta>
     <meta name="keywords" content="charlie hewitt"></meta>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Styles/normalize.css" type="text/css">
