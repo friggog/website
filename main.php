4ca4cb048a64464cb9eee34c9b8ea766580cabf0
Charlie Hewitt <contact@chewitt.me>
2021-10-09 14:49:40 +0100

    Change overscroll coloring

--- main.php
+++ main.php
@@ -60,7 +60,7 @@ $social_icons = array(
     <script src="Scripts/main.js" type="text/javascript"></script>
 </head>
 
-<body onload="scrollSetup();">
+<body>
     <header>
         <div class="content">
             <div class="title">
